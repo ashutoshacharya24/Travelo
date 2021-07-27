a84165717e8298a6d97b2f757df68d803c859978
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-07-27 11:47:59 +0530

    :rocket: Added authentication :rocket:

--- travelobackend/authentication/urls.py
+++ travelobackend/authentication/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('me/', views.me, name='me'),
 ]
--- travelobackend/authentication/views.py
+++ travelobackend/authentication/views.py
@@ -7,17 +7,17 @@ from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from authentication.serializers import UserAuthSerializer
 from django.core.validators import validate_email
+from django.contrib.auth import authenticate, login, logout
 
 
-@api_view(['GET'])
-def register(request):
+@api_view(['POST'])
+def register_view(request):
     data = request.data
     username = data.get('username')
     email = data.get('email')
     name = data.get('name')
     password = data.get('password')
 
-    email_valid_check_result = validate_email(email)
     messages = {'errors': []}
 
     if username == None:
@@ -26,10 +26,9 @@ def register(request):
         messages['errors'].append('Email can\'t be empty')
     if name == None:
         messages['errors'].append('Name can\'t be empty')
-    if not email_valid_check_result == email:
-        messages['errors'].append(email_valid_check_result)
     if password == None:
         messages['errors'].append('Password can\'t be empty')
+
     if User.objects.filter(email=email).exists():
         messages['errors'].append("Account already exists with this email id.")
     if User.objects.filter(username__iexact=username).exists():
@@ -39,13 +38,45 @@ def register(request):
         return Response({"detail": messages['errors']}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
-        user = User.objects.create(
-            username=username,
-            email=email,
-            name=name,
-            password=make_password(password)
-        )
+        user = User.objects.create(username=username, email=email, name=name, password=make_password(password))
         serializer = UserAuthSerializer(user, many=False)
     except Exception as e:
         return Response({'detail': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def login_view(request):
+    data = request.data
+    username = data.get('username')
+    password = data.get('password')
+
+    if '@' in username:
+        kwargs = {'email': username}
+    else:
+        kwargs = {'username': username}
+
+    try:
+        user = User.objects.get(**kwargs)
+        if user.check_password(password):
+            serializer = UserAuthSerializer(user, many=False)
+            login(request, user)
+    except User.DoesNotExist:
+        return Response({'detail': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def logout_view(request):
+    logout(request)
+    return Response({'detail': 'Logout success'}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def me(request):
+    if request.user.is_authenticated:
+        serializer = UserAuthSerializer(request.user)
+        return Response({'detail': 'User is authenticated', 'user': serializer.data})
+    else:
+        return Response({'detail': 'Authentication not found'}, status=status.HTTP_400_BAD_REQUEST)
